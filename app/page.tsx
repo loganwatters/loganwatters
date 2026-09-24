4ad677f4dc29791c440f4d5b9b9ac024c1988f79
Logan Watters <watte103@umn.edu>
2026-09-23 20:26:04 -0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { BlogPosts } from 'app/components/posts'
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Logan Watters
+      </h1>
+      <p className="mb-4">
+        {`I'm an Embedded Software Engineer. `}
+      </p>
+    </section>
+  )
+}
